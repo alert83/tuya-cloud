173f160f0038261d49a4f8a3577d569e90ee38ae
Yevgeniy Li <alert83@gmail.com>
2020-05-28 18:36:55 +0600

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const gulpCopy = require('gulp-copy');
-const sourceFiles = ['src/*.html', 'src/*.json'];
-const destination = 'build/';
+const sourceFiles = ['./src/index.html'];
+const destination = './build/';
 
 gulp.task('default', function () {
   return gulp
