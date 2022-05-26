7e8b7bebec44601f26fc606da2830f011fdcfcb3
Yevgeniy Li <alert83@gmail.com>
2022-05-27 01:43:30 +0600

    up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import {TuyaApi} from "./class";
 import {isEmpty} from "lodash";
+import {TuyaApi} from "./class";
+import TuyaMessageSubscribeWebsocket from "./events";
 
 module.exports = (RED) => {
     function configuration(config) {
@@ -115,46 +116,66 @@ module.exports = (RED) => {
     }
 
     RED.nodes.registerType('tuya-cloud-api-token', token);
+
     //
 
     function events(config) {
         RED.nodes.createNode(this, config);
         const node = this;
         const conf = RED.nodes.getNode(config.config);
-        const nodeContext = this.context();
+        // const nodeContext = this.context();
 
-        node.on('input', async (msg, send, done) => {
-            const {url, data} = msg.payload;
+        const client = new TuyaMessageSubscribeWebsocket({
+            accessId: conf.clientId,
+            accessKey: conf.secret,
+            url: TuyaMessageSubscribeWebsocket.URL.EU,
+            env: TuyaMessageSubscribeWebsocket.env.TEST,
+            maxRetryTimes: 100,
+        });
 
-            // try {
-            //     const client = TuyaApi.getInstance({
-            //         clientId: conf.clientId,
-            //         secret: conf.secret,
-            //         schema: conf.schema,
-            //         region: conf.region,
-            //         handleToken: true,
-            //     });
-            //
-            //     msg.payload = await client.getAndRefreshToken();
-            //
-            //     // tslint:disable-next-line:only-arrow-functions
-            //     send = send || function () {
-            //         node.send.apply(node, arguments);
-            //     };
-            //     send(msg);
-            //
-            //     if (done) done();
-            // } catch (e) {
-            //     const err = `Error Requesting: ${JSON.stringify([e.code, e.message])}`
-            //
-            //     if (done) {
-            //         // Node-RED 1.0 compatible
-            //         done(err);
-            //     } else {
-            //         // Node-RED 0.x compatible
-            //         node.error(err, msg);
-            //     }
-            // }
+        node.on('input', async (msg) => {
+            client.open(() => {
+                console.log('open');
+                node.status({fill: "green", shape: "dot", text: 'open'});
+            });
+
+            client.message((ws, message) => {
+                client.ackMessage(message.messageId);
+                // console.log('message');
+                // console.dir(message, {depth: 10});
+
+                node.status({fill: "blue", shape: "dot", text: 'message'});
+            });
+
+            client.reconnect(() => {
+                console.log('reconnect');
+                node.status({fill: "green", shape: "dot", text: 'reconnect'});
+            });
+
+            client.ping(() => {
+                console.log('ping');
+                node.status({fill: "blue", shape: "dot", text: 'ping'});
+            });
+
+            client.pong(() => {
+                console.log('pong');
+                node.status({fill: "blue", shape: "dot", text: 'pong'});
+            });
+
+            client.close((ws, ...args) => {
+                console.log('close', ...args);
+                node.status({fill: "red", shape: "dot", text: 'close'});
+            });
+
+            client.error((ws, error) => {
+                console.log('error', error);
+                node.status({fill: "red", shape: "dot", text: 'error: ' + error});
+            });
+
+            client.start();
+            node.status({fill: "gray", shape: "dot", text: 'start'});
+
+            node.send(msg);
         });
 
     }
